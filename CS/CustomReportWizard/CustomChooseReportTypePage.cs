20253ae8dd4401d23730a2eb4dc769e992308d5a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ CS/CustomReportWizard/CustomChooseReportTypePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using DevExpress.DataAccess.Sql;
+using DevExpress.DataAccess.Wizard;
+using DevExpress.DataAccess.Wizard.Services;
+using DevExpress.Entity.ProjectModel;
+using DevExpress.XtraReports.Wizards;
+using DevExpress.XtraReports.Wizards.Presenters;
+using DevExpress.XtraReports.Wizards.Views;
+
+namespace CustomReportWizard {
+    class CustomChooseReportTypePage : ChooseReportTypePage<XtraReportModel> {
+        public CustomChooseReportTypePage(IChooseReportTypePageView view, IConnectionStorageService connectionStorageService,
+            DataSourceTypes dataSourceTypes, IWizardRunnerContext context, ISolutionTypesProvider solutionTypesProvider) : 
+            base(view, connectionStorageService, dataSourceTypes, context, solutionTypesProvider) { }
+
+        public override bool MoveNextEnabled {
+            get { return true; }
+        }
+
+        public override Type GetNextPageType() {
+            if(View.ReportType == ReportType.Standard || View.ReportType == ReportType.Empty)
+                return typeof(ChoosePageSettingsPage<CustomReportModel>);
+            return base.GetNextPageType();
+        }
+    }
+}
